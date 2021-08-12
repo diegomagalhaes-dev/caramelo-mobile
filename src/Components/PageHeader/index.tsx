d770b521639c16512e29dc2af8208375c8e9b23d
DiegoMagalhaesProgrammer <digengup@gmail.com>
2021-08-11 23:56:22 -0300

    :hammer: finallys config

--- src/Components/PageHeader/index.tsx
+++ src/Components/PageHeader/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { View, Text, Image } from "react-native";
 import { BorderlessButton } from "react-native-gesture-handler";
 import EStyleSheet from "react-native-extended-stylesheet";
-import { Feather } from "@expo/vector-icons";
+import { Octicons, FontAwesome } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/native";
 
 import backIcon from "../../images/arrowBack.png";
@@ -28,12 +28,19 @@ const PageHeader = ({ title, subtitle, image, back }: PageHeaderProps) => {
   return (
     <View style={styles.container}>
       <View style={styles.topBar}>
-        <BorderlessButton onPress={handleGoback}>
-          <Image source={backIcon} resizeMode="contain" />
+        <BorderlessButton rippleColor="#219EBC" onPress={handleGoback}>
+          <FontAwesome
+            name="angle-left"
+            size={26}
+            color="rgba(255,255,255, .7)"
+          />
         </BorderlessButton>
         {back && (
-          <BorderlessButton onPress={handleGobackToHomePage}>
-            <Feather name="x" size={22} color="#F25050" resizeMode="contain" />
+          <BorderlessButton
+            rippleColor="#219EBC"
+            onPress={handleGobackToHomePage}
+          >
+            <Octicons name="x" size={20} color="rgba(255,255,255, .7)" />
           </BorderlessButton>
         )}
       </View>
@@ -49,28 +56,29 @@ export default PageHeader;
 
 const styles = EStyleSheet.create({
   container: {
-    paddingVertical: "1.5rem",
-    backgroundColor: "#77A7C2",
+    paddingVertical: "2rem",
     alignItems: "center",
+    backgroundColor: "#7FBFDD",
   },
   topBar: {
     flexDirection: "row",
-    width: "92%",
+    width: "90%",
     alignItems: "center",
     justifyContent: "space-between",
   },
   title: {
-    fontFamily: "MPLUSRounded1c_700Bold",
+    fontFamily: "MPLUSRounded1c_800ExtraBold",
     color: "#ffff",
-    fontSize: "1.3rem",
+    fontSize: "1.25rem",
     lineHeight: "1.8rem",
   },
   subtitle: {
     marginTop: ".2rem",
-    fontSize: ".9rem",
+    fontSize: "1rem",
     fontFamily: "MPLUSRounded1c_500Medium",
     color: "#FFFFFF",
     opacity: 0.8,
+    maxWidth: "15rem",
   },
   content: {
     marginTop: ".8rem",
